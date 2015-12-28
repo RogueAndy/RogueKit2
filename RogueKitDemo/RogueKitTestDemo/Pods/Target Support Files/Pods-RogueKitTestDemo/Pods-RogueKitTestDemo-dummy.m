2420fbaa474ffcd1af2b0ffb08f1160f91d2c7e6
RogueAndy <619455850@qq.com>
2015-12-28 09:39:17 +0800

    dawdwa

---
+++ RogueKitDemo/RogueKitTestDemo/Pods/Target Support Files/Pods-RogueKitTestDemo/Pods-RogueKitTestDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RogueKitTestDemo : NSObject
+@end
+@implementation PodsDummy_Pods_RogueKitTestDemo
+@end
---
+++ RogueKitDemo/RogueKitTestDemo/RogueKitTestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RogueKitTestDemo
+//
+//  Created by Rogue on 15/12/23.
+//  Copyright © 2015年 Rogue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
