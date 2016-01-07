56fad32ac9b4afe094902be888b58cbbba300ea4
RogueAndy <619455850@qq.com>
2016-01-07 15:23:34 +0800

    2016-01-07 删除Demo

--- RogueKitDemo/RogueKitTestDemo/DerivedData/RogueKitTestDemo/Build/Intermediates/RogueKitTestDemo.build/Release-iphonesimulator/RogueKitTestDemo.build/Script-AAE8BF5F5B518B3DD3913071.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-"${SRCROOT}/Pods/Target Support Files/Pods-RogueKitTestDemo/Pods-RogueKitTestDemo-frameworks.sh"
-
--- RogueKitDemo/RogueKitTestDemo/Pods/Target Support Files/Pods-RogueKitTestDemo/Pods-RogueKitTestDemo-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RogueKitTestDemo : NSObject
-@end
-@implementation PodsDummy_Pods_RogueKitTestDemo
-@end
