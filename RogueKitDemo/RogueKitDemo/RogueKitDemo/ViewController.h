22dbabfbfa50fccf13129db2ee90722e5a1e64b8
RogueAndy <619455850@qq.com>
2016-01-07 15:29:07 +0800

    新建

---
+++ RogueKitDemo/RogueKitDemo/RogueKitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RogueKitDemo
+//
+//  Created by Rogue on 16/1/7.
+//  Copyright © 2016年 Rogue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RogueKitDemo/RogueKitDemo/RogueKitDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  RogueKitDemo
+//
+//  Created by Rogue on 16/1/7.
+//  Copyright © 2016年 Rogue. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
